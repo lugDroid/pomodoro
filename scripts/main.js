03a6303e6dac76d48b32d7b661121de28492af26
lugDroid <diazbastos@gmail.com>
2018-10-16 15:20:30 +0200

    Structured as modules

--- scripts/main.js
+++ scripts/main.js
@@ -2,15 +2,17 @@
   'use strict';
 
   // PRESETS MODULE
-  function Preset(id, addButtonId, subButtonId, defaultValue) {
-    var value = defaultValue;
+  function Preset(id, addButtonId, subButtonId) {
+
+    // Private variables
+    var value;
     var MIN = 5;
     var MAX = 55;
     var CHANGE = 5;
     var addBtn = document.getElementById(addButtonId);
     var subBtn = document.getElementById(subButtonId);
 
-    // buttons events
+    // Buttons events
     addBtn.onclick = function(e) {
       increment();
       update();
@@ -21,111 +23,154 @@
       update();
     };
 
-    function increment() {
+    // Functions
+    var init = function(defaultValue) {
+      value = defaultValue;
+      update();
+    };
+
+    var increment = function() {
       if (value < MAX) {
         value += CHANGE;
       }
-    }
+    };
 
-    function decrement() {
+    var decrement = function() {
       if (value > MIN) {
         value -= CHANGE;
       }
-    }
-
-    function update() {
-      document.getElementById(id).innerHTML = value;
-    }
-
-    var publicAPI = {
-      value: value,
-      update: update
     };
 
-    return publicAPI;
+    var update = function() {
+      document.getElementById(id).innerHTML = value;
+    };
+
+    var getValue = function() {
+      return value;
+    };
+
+    // Public API
+    return {
+      getValue: getValue,
+      init: init,
+      update: update
+    };
   }
 
   // TIMER MODULE
   // preset and cycle in ms
-  function Timer(preset, cycle, startBtnId, timerId) {
+  function Timer() {
+
+    // Private variables
     var running;
     var interval;
-    var value = preset;
-    var startBtn = document.getElementById(startBtnId);
-    var timer = document.getElementById(timerId);
+    var value;
+    var cycle;
+    var element;
 
-    // start button event
-    startBtn.onclick = function(e) {
-      if (!running) {
-        start();
-        e.target.innerHTML = 'PAUSE';
-      } else {
-        pause();
-        e.target.innerHTML = 'START';
+    // Functions
+    var init = function(newCycle, timerId) {
+      cycle = newCycle;
+      element = document.getElementById(timerId);
+    };
+
+    var start = function(preset) {
+      value = preset;
+      running = true;
+      interval = setInterval(update, cycle);
+    };
+
+    var pause = function() {
+      running = false;
+      clearInterval(interval);
+    };
+
+    var update = function() {
+      if (timer.isRunning() && !timer.isFinished()) {
+        timer.setValue(timer.getValue() - timer.getCycle());
+        element.innerHTML = timer.getMinutes() + ':' + timer.getSeconds();
       }
     };
 
-    function start() {
-      running = true;
-      interval = setInterval(update, cycle);
-    }
-
-    function pause() {
-      running = false;
-      clearInterval(interval);
-    }
-
-    function update() {
-      if (running) {
-        value = value - cycle;
-        timer.innerHTML = getMinutes() + ':' + getSeconds();
-      }
-    }
-
-    function isFinished() {
+    var isFinished = function() {
       if (value == 0 && running) {
         return true;
       } else {
         return false;
       }
-    }
+    };
 
-    function getMinutes() {
+    var isRunning = function() {
+      return running;
+    };
+
+    var getMinutes = function() {
       return pad(Math.trunc(value / 60000), 2);
-    }
+    };
 
-    function getSeconds() {
+    var getSeconds = function() {
       return pad(value % 60000 / 1000, 2);
-    }
+    };
 
-    function pad(num, length) {
+    var pad = function(num, length) {
       var str = num +  '';
       while (str.length < length) {
         str = '0' + str;
       }
 
       return str;
-    }
+    };
 
-    var publicAPI = {
-      running: running,
+    var getValue = function() {
+      return value;
+    };
+
+    var setValue = function(newValue) {
+      value = newValue;
+    };
+
+    var getCycle = function() {
+      return cycle;
+    };
+
+    // Public API
+    return {
+      isRunning: isRunning,
+      init: init,
       start: start,
       pause: pause,
       update: update,
       isFinished: isFinished,
       getMinutes: getMinutes,
-      getSeconds: getSeconds
+      getSeconds: getSeconds,
+      getValue: getValue,
+      setValue: setValue,
+      getCycle: getCycle
     };
-
-    return publicAPI;
   }
 
   // MAIN
-  var pomodoroPreset = Preset('pomodoro', 'add-pom-btn', 'sub-pom-btn', 25);
-  var restPreset = Preset('rest', 'add-rest-btn', 'sub-rest-btn', 5);
-  var timer = Timer(pomodoroPreset.value * 60000, 1000, 'start-stop', 'timer');
+  var pomodoroPreset = Preset('pomodoro', 'add-pom-btn', 'sub-pom-btn');
+  var restPreset = Preset('rest', 'add-rest-btn', 'sub-rest-btn');
+  var timer = Timer();
+  var startBtn = document.getElementById('start-stop');
 
-  pomodoroPreset.update();
-  restPreset.update();
+  // Initialize timer
+  timer.init(1000, 'timer');
+
+  // Initialize presets with defaults values
+  pomodoroPreset.init(25);
+  restPreset.init(5);
+
+  // Start button event
+  startBtn.onclick = function(e) {
+    if (!timer.running) {
+      timer.start(pomodoroPreset.getValue() * 60000);
+      e.target.innerHTML = 'PAUSE';
+    } else {
+      timer.pause();
+      e.target.innerHTML = 'START';
+    }
+  };
 
 })();
